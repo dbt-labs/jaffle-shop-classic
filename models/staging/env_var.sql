fe47cebb30f42f98c826f8066321cf0bdc6bab6d
Julia Schottenstein <julia.schottenstein@fishtownanalytics.com>
2021-07-21 20:31:06 +0000

    change

--- models/staging/env_var.sql
+++ models/staging/env_var.sql
@@ -1,7 +1,8 @@
 with env_var as (
-    '{{ env_var("DBT_CLOUD_PROJECT_ID") }}' as project_id,
-    '{{ env_var("DBT_CLOUD_JOB_ID") }}' as job_id,
-    '{{ env_var("DBT_CLOUD_RUN_ID") }}' as run_id
+    select
+        '{{ env_var("DBT_CLOUD_PROJECT_ID") }}' as project_id,
+        '{{ env_var("DBT_CLOUD_JOB_ID") }}' as job_id,
+        '{{ env_var("DBT_CLOUD_RUN_ID") }}' as run_id
 )
 
 select * from env_var
