8b695167142841e7eed0ac8d66c42860f18f5c15
swanderz <swanson.anders@gmail.com>
2021-09-07 11:09:30 -0700

    not all dbs support 'using'

--- models/customers.sql
+++ models/customers.sql
@@ -38,7 +38,8 @@ customer_payments as (
 
     from payments
 
-    left join orders using (order_id)
+    left join orders on
+         payments.order_id = orders.order_id
 
     group by 1
 
@@ -57,9 +58,11 @@ final as (
 
     from customers
 
-    left join customer_orders using (customer_id)
+    left join customer_orders
+        on customers.customer_id = customer_orders.customer_id
 
-    left join customer_payments using (customer_id)
+    left join customer_payments
+        on  customers.customer_id = customer_payments.customer_id
 
 )
 
--- models/orders.sql
+++ models/orders.sql
@@ -47,7 +47,8 @@ final as (
 
     from orders
 
-    left join order_payments using (order_id)
+    left join order_payments
+        on orders.order_id = order_payments.order_id
 
 )
 
