ebd2cf7f2ea9acdb44b01ee3dfb002240d07a107
swanderz <swanson.anders@gmail.com>
2021-11-09 11:20:33 -0800

    drop old files

--- macros/helpers/dropif.sql
+++
@@ -1,9 +0,0 @@
-{% macro firebolt__dropif(node) %}
-    
-    {% set ddl %}
-        drop table if exists {{source(node.source_name, node.name)}}
-    {% endset %}
-    
-    {{return(ddl)}}
-
-{% endmacro %}
