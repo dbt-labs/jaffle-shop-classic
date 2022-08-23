285ba2d5eaecc5d6697aa68d64cb996041e127e6
Kishore Bandi <bandikishores@gmail.com>
2022-08-23 14:22:28 +0530

    Update orders.sql

--- models/orders.sql
+++ models/orders.sql
@@ -16,12 +16,10 @@ order_payments as (
 
     select
         order_id,
-
-        {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
-        {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}
-        {% endfor -%}
-
+        sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
+        sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
+        sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
+        sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount,
         sum(amount) as total_amount
 
     from payments
@@ -37,6 +35,10 @@ final as (
         orders.customer_id,
         orders.order_date,
         orders.status,
+        order_payments.credit_card_amount,
+        order_payments.coupon_amount,
+        order_payments.bank_transfer_amount,
+        order_payments.gift_card_amount,
         order_payments.total_amount as amount
 
     from orders
