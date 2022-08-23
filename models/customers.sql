0930fe00ae6a9fde7584954259c7ab1c4e36a5c5
Kishore Bandi <bandikishores@gmail.com>
2022-08-23 18:10:07 +0530

    Update customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -71,7 +71,7 @@ final as (
     left join customer_orders
         on customers.customer_id = customer_orders.customer_id
 
-    left join customer_payments
+    left join amount_per_customer
         on  customers.customer_id = customer_payments.customer_id
 
 )
