ce8d3274230f66c70759bc8467a28be274b0e36b
ahoroshko <ahoroshko@shutterstock.com>
2023-01-19 10:07:08 +0200

    -

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,4 +1,4 @@
-{% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
+  {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
 
 with orders as (
 
