1d3e6f0ff05236a0695bff9d6963b677f9751ff4
ofir <ofir@foundational.io>
2023-01-22 10:05:23 +0200

    count payment_methods.

--- models/orders.sql
+++ models/orders.sql
@@ -22,7 +22,8 @@ order_payments as (
         order_id,
 
         {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
+        count(payment_method) as {{ payment_method }}_count
         {% endfor -%}
 
         sum(amount) as total_amount
