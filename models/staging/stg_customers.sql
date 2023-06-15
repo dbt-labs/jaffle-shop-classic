7ceefc5d5b6cb22327d9c2081bc4469198e766c0
popcorny <celu@infuseai.io>
2023-06-15 16:32:29 +0800

    Source migration
    Signed-off-by: popcorny <celu@infuseai.io>

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -4,14 +4,14 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_customers') }}
+    select * from {{ ref('raw_customers_v2') }}
 
 ),
 
 renamed as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
