34ce7793b82022ab8905471164f21b7079998006
Priya Chauhan <pchauhan@phdata.io>
2023-07-25 17:40:55 +0000

    developed customer model, orders model and generate docs

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -4,15 +4,15 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_orders') }}
+    select * from user_pchauhan.jaffle_shop.orders
 
 ),
 
 renamed as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
