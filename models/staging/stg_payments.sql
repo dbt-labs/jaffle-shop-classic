fb62f72239456160bc0c8b5e368baf0163a099f5
natetongwong@gmail.com <natetongwong@gmail.com>
2023-08-02 18:29:01 +0000

    ❇️ New     - Job        - TestAirflow
    🔁 Modified
        - Model
           - customers
           - stg_payments

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,25 +1,28 @@
-with source as (
-    
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_payments') }}
+WITH source AS (
+
+  {#-
+  Normally we would select from the table here, but we are using seeds to load
+  our data in this project
+  #}
+  SELECT * 
+  
+  FROM {{ ref('raw_payments')}}
 
 ),
 
-renamed as (
+renamed AS (
 
-    select
-        id as payment_id,
-        order_id,
-        payment_method,
-
-        -- `amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
-
-    from source
+  SELECT 
+    id AS payment_id,
+    order_id,
+    payment_method,
+    -- `amount` is currently stored in cents, so we convert it to dollars
+    amount / 100 AS amount
+  
+  FROM source
 
 )
 
-select * from renamed
+SELECT *
+
+FROM renamed
