059bde2d387d1c671718b697cdb1e68e846484e3
MajaCernja <maja.cernja@krakentechnologies.ltd>
2023-12-30 12:09:15 +0000

    Add exposures

--- jaffle_shop/models/final/finance/fnl_finance_returnsvalue.sql
+++ jaffle_shop/models/final/finance/fnl_finance_returnsvalue.sql
@@ -1,7 +1,7 @@
 WITH fnl_finance_returnsvalue AS (
     SELECT
-        orders.customer_id
-        , SUM(orders.amount) AS returns_value
+        orders.customer_id AS customer_id
+        , SUM(orders.amount) AS total_return_value
     FROM {{ref('wh_jaffle_orders')}} as orders
     WHERE orders.status = 'returned'
     GROUP BY orders.customer_id
