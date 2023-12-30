a005a58790be0fc504ac8e3c9d46e9171c58ffca
MajaCernja <maja.cernja@krakentechnologies.ltd>
2023-12-30 11:48:27 +0000

    Create finance returns value table

--- jaffle_shop/models/final/finance/fnl_finance_returnsvalue.sql
+++ jaffle_shop/models/final/finance/fnl_finance_returnsvalue.sql
@@ -1,3 +1,10 @@
 WITH fnl_finance_returnsvalue AS (
     SELECT
+        orders.customer_id
+        , SUM(orders.amount) AS returns_value
+    FROM {{ref('wh_jaffle_orders')}} as orders
+    WHERE orders.status = 'returned'
+    GROUP BY orders.customer_id
 )
+
+SELECT * FROM fnl_finance_returnsvalue
