43c5aa15f262837246739cd54b6fed31142cc9e5
Montara <montara@montara.io>
2024-04-14 17:09:53 +0000

    Merging branch d84e5fbf-961d-408a-909a-fab5a3582da4 to montara_main

--- models/montara/models/cleansed_listings.sql
+++ models/montara/models/cleansed_listings.sql
@@ -1,6 +1,13 @@
 --start_query
 WITH RAW_LISTINGS AS ( SELECT * FROM {{ source('RAW_LISTINGS', 'RAW_LISTINGS') }})
 
-select * from raw_listings
+SELECT * FROM ( 
+  select * from raw_listings
+) AS montara_model
+--start_incremental
+{% if is_incremental() %}
+  WHERE updated_at >= (SELECT max(updated_at) FROM {{ this }})
+{% endif %}
+--end_incremental
 --original_sql
---select * from raw_listings 
+--select * from raw_listings
