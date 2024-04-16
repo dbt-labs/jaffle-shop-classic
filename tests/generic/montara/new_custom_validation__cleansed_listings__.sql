4ee07b6aded0ebcc347035c728f0ae95caf4d050
Montara <montara@montara.io>
2024-04-16 11:09:42 +0000

    Merging branch d84e5fbf-961d-408a-909a-fab5a3582da4 to montara_main

---
+++ tests/generic/montara/new_custom_validation__cleansed_listings__.sql
@@ -0,0 +1,10 @@
+{% test new_custom_validation__cleansed_listings__(model) %}
+--start_test_query
+--start_query
+WITH orders AS (SELECT * FROM {{ ref('orders') }})
+
+select * from orders
+--original_sql
+--select * from orders
+--end_test_query
+{% endtest %}
