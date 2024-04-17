7d492bb640685e3199ff3cf28435251195ef5bab
Montara <montara@montara.io>
2024-04-17 14:29:48 +0000

    Merging branch d84e5fbf-961d-408a-909a-fab5a3582da4 to montara_main

--- tests/generic/montara/test_something__cleansed_listings__.sql
+++ tests/generic/montara/test_something__cleansed_listings__.sql
@@ -1,4 +1,4 @@
-{% test new_custom_validation__cleansed_listings__(model) %}
+{% test test_something__cleansed_listings__(model) %}
 --start_test_query
 --start_query
 WITH orders AS (SELECT * FROM {{ ref('orders') }})
