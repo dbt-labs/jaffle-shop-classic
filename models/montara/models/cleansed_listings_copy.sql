f21e26e594ffd080aa93bbf39e616cec7d832f99
Montara <montara@montara.io>
2024-05-09 06:46:00 +0000

    Merging branch c01710d2-9c07-4224-bc14-31d67f553811 to montara_main

---
+++ models/montara/models/cleansed_listings_copy.sql
@@ -0,0 +1,22 @@
+--start_query
+
+SELECT * FROM ( 
+  select
+*
+from
+raw_listings
+where
+id is not null
+) AS montara_model
+--start_incremental
+{% if is_incremental() %}
+  WHERE updated_at >= (SELECT max(updated_at) FROM {{ this }})
+{% endif %}
+--end_incremental
+--original_sql
+--select
+--  *
+--from
+--  raw_listings
+--where
+--  id is not null
