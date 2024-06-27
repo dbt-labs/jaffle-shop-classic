8c3949c7ce7c76140143a8dedcf534a5bee12ff6
TCronino <166496893+TCronino@users.noreply.github.com>
2024-06-27 18:15:25 +0100

    setting up schedule

--- jaffle_dagster/jaffle_dagster/schedules.py
+++ jaffle_dagster/jaffle_dagster/schedules.py
@@ -6,10 +6,10 @@ from dagster_dbt import build_schedule_from_dbt_selection
 from .assets import jaffle_shop_dbt_assets
 
 schedules = [
-#     build_schedule_from_dbt_selection(
-#         [jaffle_shop_dbt_assets],
-#         job_name="materialize_dbt_models",
-#         cron_schedule="0 0 * * *",
-#         dbt_select="fqn:*",
-#     ),
+     build_schedule_from_dbt_selection(
+         [jaffle_shop_dbt_assets],
+         job_name="materialize_dbt_models",
+         cron_schedule="0 0 * * *",
+         dbt_select="fqn:*",
+     ),
 ]
